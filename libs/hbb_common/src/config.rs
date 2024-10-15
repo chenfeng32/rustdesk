d92d185e44e77a60f2a5e1322a5cd43af18add63
PIUNI <125922996+chenfeng32@users.noreply.github.com>
2024-10-15 18:26:17 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rds.piuni.cn"];
+pub const PUBLIC_RS_PUB_KEY: &str = "KrUEnBz1gIY4MaZ4yNnIddCn9sLvrH8BL1dC+KQm6EE=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
